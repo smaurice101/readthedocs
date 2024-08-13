a21924d7c84803fe8ca37fb7a43704a847367570
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-08-13 18:03:37 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.autosectionlabel'
 ]
 
 intersphinx_mapping = {
